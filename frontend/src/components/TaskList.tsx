e1e7fbf8b641f2c5ec9fd05523794ec4a188d96d
nwassom <newassom@gmail.com>
2024-10-04 12:21:44 -0400

    Made delete UI better

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -54,7 +54,7 @@ const TaskList: React.FC = () =>
 					{ tasks.map((task) =>
 						(
 							<li key={task.id} 
-								className="bg-white shadow-lg shadow-amber-200/60 rounded-lg p-8 shadow-sm min-w-[300px] max-w-[300px] flex-shrink-0">
+								className="TaskBox bg-white shadow-lg shadow-amber-200/60 rounded-lg p-8 shadow-sm min-w-[300px] max-w-[300px] flex-shrink-0">
 								<div className="deleteTask">
 									<DeleteTask task={task}/>
 								</div>
