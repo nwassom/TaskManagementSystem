4167605264f0be13c537a3d06a55f5bfff40401d
nwassom <newassom@gmail.com>
2024-10-15 13:04:03 -0400

    Not sure if much changed git wise finally fixed docker sql

--- backend/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb464f03bc65fb19f27bd5619ef6e58d2319f91b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce13b9a77d224eab1c8037c313fcf5b5b62baff")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
