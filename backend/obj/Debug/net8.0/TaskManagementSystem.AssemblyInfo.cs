27a85aefbbcf93b543d234f5f830064a6403f5d9
nwassom <nwassom@outlook.com>
2024-10-16 12:55:27 -0400

    Added azure folder and terraform

--- backend/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/TaskManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce13b9a77d224eab1c8037c313fcf5b5b62baff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4167605264f0be13c537a3d06a55f5bfff40401d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
